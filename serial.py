01afcc865759242d7a6fc1807c352fa35081b0df
Max Jeon <maxjeon97@gmail.com>
2024-02-20 15:13:02 -0800

    Finish serial.py and working on wordfinder.py

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Creates a serial number generator that starts at a number, start"""
+        self.start = start
+        self.generated_num = start - 1
+
+    def __repr__(self):
+        return f"<Serial number generator that starts at {self.start}>"
+
+    def generate(self):
+        """Returns the next sequential number"""
+        self.generated_num += 1
+        return self.generated_num
+
+    def reset(self):
+        """Resets the number back to the original start number"""
+        self.generated_num = self.start - 1
+
+
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,19 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        """Stores the file path"""
+        self.file_path = path
+        self.word_list = self.make_list()
+        print(f"{len(self.word_list)} words read")
+
+    def make_list(self):
+        """Opens the file, splits the lines into a list and return said list"""
+        file = open(self.file_path)
+        word_list = file.splitlines()
+        file.close()
+        return word_list
+
+
+
+
