1f0f80d05c4a744d27531da12a7fae3d353a169e
Adrian McBroom <adrianmcb2000@gmail.com>
2024-02-20 16:16:03 -0800

    after second code review

--- wordfinder.py
+++ wordfinder.py
@@ -1,19 +1,48 @@
+from random import choice
+
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, path):
-        """Stores the file path"""
+        """Stores the file path, creates a list of words from the file, and
+        prints the amount of words read from that file"""
         self.file_path = path
-        self.word_list = self.make_list()
+
+        file = open(self.file_path)
+        self.word_list = self.make_list(file)
+
+        file.close()
+
         print(f"{len(self.word_list)} words read")
 
-    def make_list(self):
-        """Opens the file, splits the lines into a list and return said list"""
-        file = open(self.file_path)
-        word_list = file.splitlines()
-        file.close()
-        return word_list
+    def __repr__(self):
+        return f"<WordFinder: holds list of words found in the file at {self.path}>"
+
+    def make_list(self, file):
+        """Takes the file, splits the lines into a list and return said list"""
+        return [line.strip() for line in file]
+
+    def random(self):
+        """Returns random word from the read-in list of words"""
+        return choice(self.word_list)
 
 
+class SpecialWordFinder(WordFinder):
+    """SpecialWordFinder: finds random words from a dictionary while filtering
+    out comments and blank lines"""
 
+#  def __init__(self, path):  # unnecessary
+#     """Stores the file path, creates a list of words from the file filtered
+#    to not include empty spaces or comments, then prints the amount of
+#   words read"""
+#      super().__init__(path)
 
+    def __repr__(self):
+        return f"""<SpecialWordFinder: holds list of words found in the file at
+        {self.path} minus any empty lines or comment lines>"""
+
+    def make_list(self, file):
+        """Takes the file, splits the lines into a list not including empty
+        lines or comment lines and returns that list"""
+        return [line.strip() for line in file if line != "\n" and not line.startswith('#')]  # for line in parent's super().make_list
